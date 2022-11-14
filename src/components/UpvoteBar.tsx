47803199975ef0331092d25fc6dd03a0f26e59b8
Eliud de León <eliud.deleon.10@gmail.com>
2022-11-13 22:41:59 -0600

    Added logic for upvote bar

--- src/components/UpvoteBar.tsx
+++ src/components/UpvoteBar.tsx
@@ -2,16 +2,48 @@ import Typography from "@mui/material/Typography";
 import { DownvoteIcon, UpvoteIcon } from "./CustomIcons";
 import { orange, blue } from "@mui/material/colors";
 import { HoverArrow, UpvoteCount, WrapperDiv } from "./UpvoteBar.style";
+import { useState } from "react";
 
 function UpvoteBar() {
   const upvoteColor = orange[500];
   const downvoteColor = blue[500];
 
+  const handleVote = (isUpvote: boolean) => {
+    if (isUpvote && currentVote !== 1) {
+      setVoteCount(voteCount - currentVote + 1);
+      setCurrentVote(1);
+    } else if (!isUpvote && currentVote !== -1) {
+      setVoteCount(voteCount - currentVote - 1);
+      setCurrentVote(-1);
+    } else if (isUpvote) {
+      setVoteCount(voteCount - 1);
+      setCurrentVote(0);
+    } else if (!isUpvote) {
+      setVoteCount(voteCount + 1);
+      setCurrentVote(0);
+    }
+  };
+
+  const [voteCount, setVoteCount] = useState(Math.floor(Math.random() * 100));
+  const [currentVote, setCurrentVote] = useState(0);
+
   return (
     <div className={WrapperDiv}>
-      <UpvoteIcon className={`${HoverArrow(upvoteColor)}`} />
-      <Typography className={`${UpvoteCount}`}>80</Typography>
-      <DownvoteIcon className={`${HoverArrow(downvoteColor)}`} />
+      <UpvoteIcon
+        onClick={() => handleVote(true)}
+        sx={{
+          color: currentVote === 1 ? upvoteColor : undefined,
+        }}
+        className={`${HoverArrow(upvoteColor)}`}
+      />
+      <Typography className={`${UpvoteCount}`}>{voteCount}</Typography>
+      <DownvoteIcon
+        onClick={() => handleVote(false)}
+        sx={{
+          color: currentVote === -1 ? downvoteColor : undefined,
+        }}
+        className={`${HoverArrow(downvoteColor)}`}
+      />
     </div>
   );
 }
